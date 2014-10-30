3f6cce0e5c60bf057c3638ea85a1ccaac45bc9b2
ulic75 <ulic75@gmail.com>
2014-10-30 10:49:01 -0600

    Comment out examples for extending the model

--- Invoicer.Web/app/services/model.js
+++ Invoicer.Web/app/services/model.js
@@ -11,39 +11,40 @@
 		return service;
 
 		function configureMetadataStore(metadataStore) {
-			registerLineItemDescription(metadataStore);
-			registerClient(metadataStore);
+			// These are just two examples which call the example functions below
+			//registerLineItemDescription(metadataStore);
+			//registerClient(metadataStore);
 		}
 
 		//#region Internal Methods
 
-		function registerClient(metadataStore) {
-			metadataStore.registerEntityTypeCtor('Client', Client);
+		//function registerClient(metadataStore) {
+		//	metadataStore.registerEntityTypeCtor('Client', Client);
 
-			function Client() { }
+		//	function Client() { }
 
-			Object.defineProperty(Client.prototype, 'fullName', {
-				get: function () {
-					var name = this.name;
-					var value = name + ' (blah)';
-					return value;
-				}
-			})
-		}
+		//	Object.defineProperty(Client.prototype, 'fullName', {
+		//		get: function () {
+		//			var name = this.name;
+		//			var value = name + ' (blah)';
+		//			return value;
+		//		}
+		//	})
+		//}
 
-		function registerLineItemDescription(metadataStore) {
-			metadataStore.registerEntityTypeCtor('LineItemDescription', LineItemDescription);
+		//function registerLineItemDescription(metadataStore) {
+		//	metadataStore.registerEntityTypeCtor('LineItemDescription', LineItemDescription);
 
-			function LineItemDescription() { }
+		//	function LineItemDescription() { }
 
-			Object.defineProperty(LineItemDescription.prototype, 'name', {
-				get: function () {
-					var description = this.description;
-					var value = description + ' (blah)';
-					return value;
-				}
-			})
-		}
+		//	Object.defineProperty(LineItemDescription.prototype, 'name', {
+		//		get: function () {
+		//			var description = this.description;
+		//			var value = description + ' (blah)';
+		//			return value;
+		//		}
+		//	})
+		//}
 
 		//#endregion
 	}
