06653ca9ea0f8ba7be726c6f12f750aa3001c447
ulic75 <ulic75@gmail.com>
2014-10-31 08:48:45 -0600

    Add filtering

--- Invoicer.Web/app/clients/clients.js
+++ Invoicer.Web/app/clients/clients.js
@@ -1,16 +1,20 @@
 ﻿(function () {
 	'use strict';
 	var controllerId = 'clients';
-	angular.module('app').controller(controllerId, ['common', 'datacontext', clients]);
+	angular.module('app').controller(controllerId, ['common', 'config', 'datacontext', clients]);
 
-	function clients(common, datacontext) {
+	function clients(common, config, datacontext) {
 		var vm = this;
+		var keyCodes = config.keyCodes;
 		var getLogFn = common.logger.getLogFn;
 		var log = getLogFn(controllerId);
 
 		vm.clients = [];
+		vm.clientSearch = '';
+		vm.filteredClients = [];
 		vm.title = 'Clients';
 		vm.refresh = refresh;
+		vm.search = search;
 
 		activate();
 
@@ -21,11 +25,28 @@
 
 		function getClients(forceRefresh) {
 			return datacontext.getClients(forceRefresh).then(function (data) {
-				return vm.clients = data;
+				vm.clients = data;
+				applyFilter();
+				return vm.clients;
 			});
 		}
 
 		function refresh() { getClients(true); }
 
+		function search($event) {
+			if ($event.keyCode === keyCodes.esc) {
+				vm.clientSearch = '';
+			}
+			applyFilter();
+		}
+
+		function applyFilter() {
+			vm.filteredClients = vm.clients.filter(clientFilter);
+		}
+
+		function clientFilter(client) {
+			var isMatch = vm.clientSearch ? common.textContains(client.name, vm.clientSearch) : true;
+			return isMatch;
+		}
 	}
 })();
--- Invoicer.Web/app/invoices/invoices.js
+++ Invoicer.Web/app/invoices/invoices.js
@@ -1,14 +1,21 @@
 ﻿(function () {
 	'use strict';
 	var controllerId = 'invoices';
-	angular.module('app').controller(controllerId, ['common', 'datacontext', invoices]);
+	angular.module('app').controller(controllerId, ['common', 'config', 'datacontext', invoices]);
 
-	function invoices(common, datacontext) {
+	function invoices(common, config, datacontext) {
 		var vm = this;
+		var keyCodes = config.keyCodes;
 		var getLogFn = common.logger.getLogFn;
 		var log = getLogFn(controllerId);
 
+		var applyFilter = function () { };
+
+		vm.filteredInvoices = [];
 		vm.invoices = [];
+		vm.invoicesFilter = invoicesFilter;
+		vm.invoicesSearch = '';
+		vm.search = search;
 		vm.title = 'Invoices';
 		vm.refresh = refresh;
 
@@ -16,16 +23,44 @@
 
 		function activate() {
 			common.activateController([getInvoices()], controllerId)
-                .then(function () { log('Activated Invoices View'); });
+                .then(function () {
+                	// createSearchThrottle uses values by convention, via it's parameters:
+                	//	vm.invoicesSearch is where the user enters the search
+                	//	vm.invoices is the original unfiltered array
+                	//	vm.filteredInvoices is the filtered array
+					//	vm.invoicesFilter is the filtering function
+                	applyFilter = common.createSearchThrottle(vm, 'invoices');
+                	if (vm.invoicesSearch) { applyFilter(true); }
+                	log('Activated Invoices View');
+                });
 		}
 
 		function getInvoices(forceRefresh) {
 			return datacontext.getInvoices(forceRefresh).then(function (data) {
-				return vm.invoices = data;
+				return vm.invoices = vm.filteredInvoices = data;
 			});
 		}
 
 		function refresh() { getInvoices(true);	}
+
+		function search($event) {
+			if ($event.keyCode === keyCodes.esc) {
+				vm.invoicesSearch = '';
+				applyFilter(true);
+			} else {
+				applyFilter();
+			}
+		}
+
+		function invoicesFilter(invoice) {
+			var textContains = common.textContains;
+			var searchText = vm.invoicesSearch;
+			var isMatch = searchText ?
+				textContains(invoice.client.name, searchText)
+					|| textContains(invoice.id, searchText)
+				: true;
+			return isMatch;
+		}
 
 	}
 })();
