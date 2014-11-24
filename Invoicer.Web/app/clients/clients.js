a300520539e084b86a0e297f56a2d5604e5dae32
ulic75 <ulic75@gmail.com>
2014-11-24 14:27:02 -0700

    Add client Active/Inactive filter

--- Invoicer.Web/app/clients/clients.js
+++ Invoicer.Web/app/clients/clients.js
@@ -9,6 +9,8 @@
 		var getLogFn = common.logger.getLogFn;
 		var log = getLogFn(controllerId);
 
+		vm.active = 1;
+		vm.activeChanged = activeChanged;
 		vm.clients = [];
 		vm.clientSearch = '';
 		vm.filteredClients = [];
@@ -24,8 +26,12 @@
                 .then(function () { log('Activated Clients View'); });
 		}
 
-		function getClients(forceRefresh) {
-			return datacontext.client.getAll(forceRefresh).then(function (data) {
+		function activeChanged() {
+			getClients();
+		}
+
+		function getClients(forceRemote) {
+			return datacontext.client.getAll(forceRemote).then(function (data) {
 				vm.clients = data;
 				applyFilter();
 				return vm.clients;
@@ -52,6 +58,7 @@
 		}
 
 		function clientFilter(client) {
+			if (Number(vm.active) != 2 && client.active != Number(vm.active)) { return false };
 			var isMatch = vm.clientSearch ? common.textContains(client.name, vm.clientSearch) : true;
 			return isMatch;
 		}
